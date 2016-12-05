3fc9f42d33bd1dba637b7c7dbbc63c88c5c3706c
Charlie Steele <steelech@umich.edu>
2016-12-05 16:34:18 -0500

    correct route transition between dashboard and music/albums

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -1,8 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	tab: '',
 	session: Ember.inject.service(),
 	actions: {
+		changeTab(tab) {
+			this.set("tab", tab);
+			this.transitionToRoute("/" + tab);
+		},
 		logout() {
 			this.get("session").invalidate();
 		},
