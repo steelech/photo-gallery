fde097f016e11131bf8405d2bde4a4b7096b5134
Charlie Steele <steelech@umich.edu>
2016-12-20 03:23:31 -0500

    Retrieve Cognito creds when s3 service init() hook is called

--- app/services/s3.js
+++ app/services/s3.js
@@ -2,40 +2,46 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
 	cognito: Ember.inject.service(),
+	init() {
+		this.setCreds();
+	},
 	uploadFiles(files) {
 		var self = this;
 		var promise = new Promise(function(resolve, reject) {
 			self.get("cognito").getCreds().then(function(creds) {
-				AWS.config.accessKeyId = creds.accessKeyId;
-				AWS.config.secretAccessKey = creds.secretAccessKey;
-				AWS.config.sessionToken = creds.sessionToken;
-				AWS.config.region = "us-west-2";
-				console.log("creds(hi):", creds);
-				var bucket = new AWS.S3({
-					Bucket: 'pics-songs'
-				});
 
 				var length = files.length;
 				for(var i = 0;i < length;i++) {
-					var params = {
-						Bucket: 'pics-songs',
-						Key: 'pics-songs' + i,
-						ContentType: files[i].type,
-						Body: files[i]
-
-					}
-					bucket.putObject(params, function(err, data) {
-						if(err) {
-							alert("err");
-						} else {
-							console.log("success");
-						}
-
-					})
+					self.uploadSingleFile(files[i]);
 				}
 				resolve({data: "suh. dude"});
 			})
 		})
 		return promise;
+	},
+	uploadSingleFile(file) {
+		var bucket = new AWS.S3();
+		var params = {
+			Bucket: 'pics-songs',
+			Key: 'pics-songs3',
+			ContentType: file.type,
+			Body: file
+		};
+		bucket.upload(params, function(err, data) {
+			if(err) {
+				alert("err");
+			} else {
+				alert("success");
+			}
+		});
+	},
+	setCreds() {
+		var self = this;
+		self.get("cognito").getCreds().then(function(creds) {
+			AWS.config.accessKeyId = creds.accessKeyId;
+			AWS.config.secretAccessKey = creds.secretAccessKey;
+			AWS.config.sessionToken = creds.sessionToken;
+			AWS.config.region = "us-west-2";
+		});
 	}
 });
