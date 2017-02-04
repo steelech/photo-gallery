6f226f7466e47e816fb9a0f42d0b84b8257d86c3
Charlie Steele <steelech@umich.edu>
2017-02-04 12:13:00 -0500

    pic-slideshow events and handlers

--- app/components/pic-slideshow.js
+++ app/components/pic-slideshow.js
@@ -2,5 +2,50 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	classNames: ['slideshow-component'],
+	album: null,
+	pics: null,
+	currentPic: null,
+	currentPicIndex: null,
+	numbPics: null,
+	init() {
+		this._super(...arguments);
+		this.set("numbPics", this.get("pics").toArray().length)
+		this.set("currentPicIndex", this.findCurrentIndex())
+		console.log("currentPicIndex: ", this.findCurrentIndex())
+	},
+	actions: {
+		nextPic() {
+			console.log("next pic")
+			if(this.get("currentPicIndex") + 1 == this.get("pics").toArray().length) {
+				this.set("currentPicIndex", 0)
+				this.set("currentPic", this.get("pics").toArray()[0].data)
 
+			} else {
+			}
+		},
+		prevPic() {
+			console.log("prev pic")
+		},
+		downloadPic() {
+			console.log("downloading pic")
+		},
+		deletePic() {
+			console.log("deleting pic")
+		}
+
+	},
+	findCurrentIndex() {
+		var picsArray = this.get("pics").toArray()
+		var length = picsArray.length
+
+		for(var i = 0;i < length;i++) {
+			var picData = picsArray[i].data
+			var currentPic = this.get("currentPic")
+			console.log("targetPic: ", picData)
+			console.log("other pic: ", currentPic)
+			if(picData.key === currentPic.key) {
+				return i + 1;
+			}
+		}
+	}
 });
--- app/controllers/dashboard/pictures/index.js
+++ app/controllers/dashboard/pictures/index.js
@@ -3,11 +3,14 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 	slideshowMode: false,
 	pics: null,
+	selectedPic: null,
 	actions: {
 		openSlideshow(pic) {
 			this.set("slideshowMode", true);
-			this.set("pics", pic)
-			console.log("opening slideshow in controller");
+			this.set("pics", this.get("model"))
+			this.set("selectedPic", pic)
+
+			console.log("model: ", this.get("model"))
 		},
 		closeSlideshow() {
 			this.set("slideshowMode", false);
